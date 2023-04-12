991fae80f8121857d02ce793ca09cb1758e50a4b
Taieb Rekik <Taieb.Rekik@mce.gouv.qc.ca>
2023-04-12 12:35:22 +0200

    initialize extension

---
+++ ext_localconf.php
@@ -0,0 +1,7 @@
+<?php
+
+defined('TYPO3') || die();
+
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig(
+    "@import 'EXT:qc_ws_preview_lang/Configuration/TSconfig/pageconfig.tsconfig'"
+);
