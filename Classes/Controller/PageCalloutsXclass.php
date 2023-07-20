60a90a3713b71e04e8635251c3ced227f5433e4e
Abderrazak Tgiti <tigitiabderrazak@gmail.com>
2023-07-20 11:08:43 +0100

    Add doc for classes

--- Classes/Controller/PageCalloutsXclass.php
+++ Classes/Controller/PageCalloutsXclass.php
@@ -1,5 +1,14 @@
 <?php
-
+/***
+ *
+ * This file is part of Qc BE domaine color project.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ *  (c) 2022 <techno@quebec.ca>
+ *
+ ***/
 namespace Qc\QcBePageLanguage\Controller;
 use Psr\Http\Message\ServerRequestInterface;
 use Sypets\PageCallouts\Xclass\PageLayoutControllerWithCallouts;
