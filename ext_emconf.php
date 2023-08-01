212903f9b965c7e511534db21f1d7fc82894e4f3
Abderrazak Tgiti <tigitiabderrazak@gmail.com>
2023-08-01 17:37:23 +0100

    update ext_emconfig

--- ext_emconf.php
+++ ext_emconf.php
@@ -12,7 +12,7 @@ $EM_CONF[$_EXTKEY] = [
     'version' => '1.0.2',
     'constraints' => [
         'depends' => [
-            'typo3' => '11.0.0-11.5.99',
+            'typo3' => '11.0.0-12.5.99',
         ],
     ],
     'autoload' => [
